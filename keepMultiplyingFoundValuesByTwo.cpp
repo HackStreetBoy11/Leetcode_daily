29446eee4d7084e8f0c22da54619eb4ba331694b
CodeByVarun <142214068+HackStreetBoy11@users.noreply.github.com>
2025-11-19 11:57:59 +0530

    keepMultiplyingFoundValuesByTwo

---
+++ keepMultiplyingFoundValuesByTwo.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        // sort the array
+        sort(nums.begin(),nums.end());
+        int temp = original;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==temp)
+            {
+                temp=temp*2;
+            }
+        }
+        return temp;
+    }
+};
